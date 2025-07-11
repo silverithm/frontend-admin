9bb4fc25264d1dc84f86a4569a59a7ea26466465
Kim Jun Hyeong <ggprgrkjh@naver.com>
2025-07-12 01:55:44 +0900

    feat: 루트 레이아웃 추가

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: '케어브이 관리자',
+  description: '케어브이 휴무 관리 시스템',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
