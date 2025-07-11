ce41c7b9c2e8daff58bea4162b132e526f95c543
Kim Jun Hyeong <ggprgrkjh@naver.com>
2025-07-12 03:04:48 +0900

    feat: 로딩 스피너 수정

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1749,12 +1749,9 @@ export default function AdminPage() {
 
             {/* 로딩 오버레이 */}
             {isProcessing && (
-                <div className="fixed inset-0 z-50">
-                    {/* 반투명 배경 */}
-                    <div className="absolute inset-0 bg-white/20"></div>
+                <div className="fixed inset-0 z-50 bg-white/20">
                     {/* 로딩 스피너 */}
                     <div className="relative flex items-center justify-center h-full">
-                        <div className="bg-white/90 p-4 rounded-full shadow-lg">
                             <svg
                                 className="animate-spin h-8 w-8 text-blue-600"
                                 xmlns="http://www.w3.org/2000/svg"
@@ -1775,7 +1772,6 @@ export default function AdminPage() {
                                 d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                             ></path>
                         </svg>
-                    </div>
                 </div>
             </div>
             )}
